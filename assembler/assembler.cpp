e0ce8141c0bdf5790a1ec6877c703ca69b783faf
Anton Lagerholm <lagget@gmail.com>
2016-05-15 17:39:41 +0200

    Implemented the first pass functionality.

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -4,6 +4,7 @@
 
 #include "parser.h"
 #include "code.h"
+#include "symbolTable.h"
 
 using std::string;
 using std::bitset;
@@ -14,10 +15,54 @@ int main()
 	bool moreCommands = true;
 	Parser parser("asm/Max.asm");
 	ofstream outFile("Max.hack");
-	//Code code;
+	Code code;
+	SymbolTable symbolTable;
+	int romAddress = 0;
 	string output;
 	
+	// First pass.
 	while(moreCommands)
+	{
+		parser.advance();
+		moreCommands = parser.hasMoreCommands();
+		if(moreCommands)
+		{
+			std::cout << std::endl << parser.currentCommand << std::endl;
+			commandType_e cmdType = parser.commandType();
+			//std::cout << cmdType << std::endl;
+			if(cmdType == A_COMMAND)
+			{
+				std::cout << romAddress << " @: " << parser.symbol() << std::endl;
+				romAddress++;
+			}
+			else if (cmdType == L_COMMAND)
+			{
+				std::cout << " (): " << parser.symbol() << std::endl;
+				symbolTable.addEntry(parser.symbol(), romAddress + 1);
+			}
+			else if(cmdType == C_COMMAND)
+			{
+				std::cout << romAddress << std::endl; 
+				std::cout << " Dest: " << parser.dest() << std::endl;
+				std::cout << " Comp: " << parser.comp() << std::endl;
+				std::cout << " Jump: " << parser.jump() << std::endl;
+				romAddress++;
+			}
+			
+			/*if(outFile.is_open())
+			{
+				outFile << romAddress << " " << parser.currentCommand << "\n";
+			}
+			else
+			{
+				std::cout << "Output file not open!" << std::endl;
+				break;
+			}*/
+		}
+	}
+	
+	// Second pass.
+	/*while(moreCommands)
 	{
 		parser.advance();
 		moreCommands = parser.hasMoreCommands();
@@ -58,7 +103,8 @@ int main()
 				break;
 			}
 		}
-	}
+	}*/
+	
 	outFile.close();
 	
 	return 0;
